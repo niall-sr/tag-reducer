2963cc35adb1a0e5a95c5f3fa1683f99612bfd00
Niall Bambury <niallbambury@MacBook-Pro-8.local>
2024-06-18 22:05:30 +0100

    update algorithm to clean tags

--- index.js
+++ index.js
@@ -1,74 +1,60 @@
-import * as sanityURLS from './assets/sanityURLS.json' assert {type: 'json'}
-import * as joURLS from './assets/joURLS.json' assert {type: 'json'}
+
 import fs from 'node:fs'
+import { parse } from 'csv'
 
-let sanityurls = sanityURLS.default;
-let jourls = joURLS.default;
+const path = './assets/recipe-tags.csv'
 
-const unmatched = Array.from(jourls);
-
-
-const matches = [];
-
-// because jo and our website construct urls differently we want to get the last unique uri and match based on that
-// it assumes uri's are unique (which they may not be)
-
-// let jourls = [
-//     "https://www.jamieoliver.com/features/category/mothers-day/", 
-//     "https://www.jamieoliver.com/recipes/category/occasion/mothers-day/"
-// ]
-// let sanityurls = [
-//     "inspiration/mothers-day", 
-//     "recipes/occasion/mothers-day"
-// ]
+let recordsToUpdate = [];
 
 
-sanityurls = sanityurls.map(el =>{
-    return {
-        fullURL: el,
-        regex: () => {     
-            if (el.match(`(inspiration)`)){
-                // Map "/inspirations/" to "/features/categories/"
-                return `(features|features\/categories)?\/${el.split('/').pop()}\/`
-            } else if (el.match(`(recipes/occasion)`)){
-                // Map "/recipes/occasion/" to "/recipes/category/occasion/"
-                return `recipes\/category\/occasion\/${el.split('/').pop()}\/`
-            } else {
-                // finally just try to map the last part of the uri to a url that ends with the same
-                return `\/${el.split('/').pop()}\/$`
-            }
+fs.createReadStream(path)
+  .pipe(parse({ delimiter: "\n", from_line: 1 }))
+  .on("data", function (row) {
+    // executed for each row of data
+
+    if(row[0].indexOf('UPDATE') > 0){
+        let str = row[0];
+        let arr = str.split(',');
+        
+        console.log('arr.length:', arr.length);
+
+        if(arr.length == 3){
+            recordsToUpdate.push({
+                old: arr[0],
+                new: arr[2].replace("UPDATE TAG: Change to be", "")
+                           .replace("UPDATE TAG: Change to ", "")
+                           .replace("Change this to", "")
+                           .replace("UPDATE TAG: ", "")
+                           .replace("Change to be", "")
+                           .replace("Change to", "")
+                           .replace("Changing to", "")
+                           .replace("Relabel to", "")
+                           .replace("Relabel as", "")
+                           .replace("(deleting recipes)", "")
+                           .trim()
+            }) 
         }
+
+        // console.log('arr[0]', arr[0]); 
     }
-})
-console.log('Total number of jamie oliver urls to migrate:', jourls.length);
+    
+  })
+  .on("error", function (error) {
+    // Handle the errors
+    console.log(error.message);
+  })
+  .on("end", function () {
 
-// Iterated through both sets of urls and try to find matches
-sanityurls.forEach((sanity) => {
-    jourls.forEach((jo, joIndex, joArray) => {
-        if(!(jo.match('videos') || (jo.match('playlist')))){
-            // Exclude stuff we haven't done yet like video pages since they can't match yet
-            if(jo.match(sanity.regex())){
-                matches.push({jo:jo,sanity: "https://dev.jamieoliver.com/" + sanity.fullURL})
-                joArray.splice(joIndex,1)
-            }
-        }
+    let obj = {
+      table: []
+    }
+  
+    recordsToUpdate.forEach(el => {
+      obj.table.push(el)
     })
-})
-
-
-// JO urls that we found a match for and the sanity urls they correspond to
-matches.forEach(el =>{
-    fs.writeFile('matched-urls.csv', el.jo + "," + el.sanity + ",\n", { flag: 'a+' }, err => {});
-})
-
-// JO urls that we have no match for
-jourls.forEach(el =>{
-    fs.writeFile('not-yet-matched-urls.csv', el + ",\n", { flag: 'a+' }, err => {});
-})
-
-console.log('Already migrated:', matches.length)
-console.log('Yet to be migrated:', jourls.length);
-
-// console.log('all matches:', matches)
-
-
+    
+    
+    const json = JSON.stringify(obj);
+    fs.writeFileSync('clean-tags.json', json);
+    
+  });
